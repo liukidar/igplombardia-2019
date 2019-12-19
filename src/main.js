858b4ea53a49e5f48de34e86de1448aff82382e0
Luca Pinchetti <pincoluca1@gmail.com>
2019-12-19 20:56:48 +0100

    Add hold directive

--- src/main.js
+++ src/main.js
@@ -6,6 +6,48 @@ import { i18n } from './locale'
 
 Vue.config.productionTip = false
 
+Vue.directive('hold', {
+	bind: function (el, binding, vNode) {
+			if (typeof binding.value !== 'function') {
+					const compName = vNode.context.name
+					let warn = `[hold:] provided expression '${binding.expression}' is not a function, but has to be`
+					if (compName) { warn += `Found in component '${compName}' ` }
+					// eslint-disable-next-line
+					console.warn(warn)
+			}
+
+			let pressTimer = null
+
+			let start = (e) => {
+					if (e.type === 'click' && e.button !== 0) {
+							return;
+					}
+					if (pressTimer === null) {
+							pressTimer = setTimeout(() => {
+									handler()
+							}, 1000)
+					}
+			}
+
+			let cancel = () => {
+					if (pressTimer !== null) {
+							clearTimeout(pressTimer)
+							pressTimer = null
+					}
+			}
+			const handler = (e) => {
+					binding.value(e)
+			}
+
+			el.addEventListener("mousedown", start)
+			el.addEventListener("touchstart", start)
+			el.addEventListener("click", cancel)
+			el.addEventListener("mouseout", cancel)
+			el.addEventListener("touchend", cancel)
+			el.addEventListener("touchcancel", cancel)
+	}
+})
+
 new Vue({
 	router,
 	store,
