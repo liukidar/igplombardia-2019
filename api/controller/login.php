f4e596f95c59fcfd9a2ccd81a455e93e1803a1eb
Luca Pinchetti <pincoluca1@gmail.com>
2020-01-03 16:14:47 +0100

    Add login controller

---
+++ api/controller/login.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../config.php';
+require_once '../model/user.php';
+
+header('Access-Control-Allow-Origin: http://localhost:8080');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: origin, content-type, accepts, X-HTTP-Method-Override');
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+
+$sql = new MySQL(DB_HOST, DB_USER, DB_PSW, DB_NAME);
+$vtm = new VTM($sql);
+$user = new User($vtm);
+
+if ($_DATA['action'] == 'login') {
+  $user->requestAuthToken($_DATA['mail'], $_DATA['password']);
+} else if ($_DATA['action'] == 'logout') {
+	$user->clearAuthToken($_SERVER['HTTP_AUTH_TOKEN']);
+} else {
+	http_response_code(400);
+}
+
+echo output();
