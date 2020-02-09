b6c0ed9b9b7435a2d70fd678b71d76d048a479fa
Matteo Ranzetti <ranzetti.matteo@gmail.com>
2020-02-09 21:47:47 +0100

    Remove English language choice

--- src/locale.js
+++ src/locale.js
@@ -8,7 +8,7 @@ Vue.use(VueI18n)
 export const i18n = new VueI18n({
   locale: 'it',//(window.navigator.userLanguage || window.navigator.language).substr(0,2),
 	messages: {
-		it,
-		en: {}
+		it
+		//,en: {}
 	}
 })
