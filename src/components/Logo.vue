d65213d9c01875577320afbc6a25b23634ddcdd9
Luca Pinchetti <pincoluca1@gmail.com>
2020-03-13 18:15:02 +0100

    Add ipg & ipa logos

---
+++ src/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="cmp-logo">
+		<img :src="images[type]" :style="{ width, height: 'auto' }"/>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['type', 'width'],
+	data: function() {
+		return {
+			images: {
+				igp: require('@/assets/imgs/igp.png'),
+				igpl: require('@/assets/imgs/igpl.png'),
+				ipa: require('@/assets/imgs/ipa.png'),
+				igpw: require('@/assets/imgs/igp-wide.jpg'),
+				igplw: require('@/assets/imgs/igpl-wide.png'),
+				ipaw: require('@/assets/imgs/ipa-wide.jpg'),
+			}
+		}
+	}
+}
+</script>
