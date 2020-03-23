91f3a92c516ca2c9fff901d93689e3f3b220c21e
Matteo Ranzetti <ranzetti.matteo@gmail.com>
2020-03-23 15:20:03 +0100

    Text: IGP Lombardia to IG Passivhaus Lombardia

--- src/lang/italian.js
+++ src/lang/italian.js
@@ -93,17 +93,17 @@ export default {
 					sbenefits: [
 						'Iscrizione e accesso iPHA per informazioni sulla Passivhaus',
 						'Sconti iPHA su partecipazione eventi, o acquisto materiale',
-						'Partecipazione gratuita a due eventi formativi annuali IGP Lombardia (del valore di € 50,00 cadauno)',
-						'Partecipazione eventi esclusivi iscritti IGP Lombardia (networking)'
+						'Partecipazione gratuita a due eventi formativi annuali IG Passivhaus Lombardia (del valore di € 50,00 cadauno)',
+						'Partecipazione eventi esclusivi iscritti IG Passivhaus Lombardia (networking)'
 					],
 					professionals: 'Professionisti € 120,00',
 					pbenefits: [
 						'Iscrizione ad € 100,00 per chi è già iscritto ed ha già versato quota anno precedente',
 						'Sconti iPHA su partecipazione eventi, o acquisto materiale',
-						'Partecipazione gratuita due eventi formativi annuali IGP Lombardia (del valore di € 50,00 cadauno)',
-						'Partecipazione eventi esclusivi iscritti IGP Lombardia (networking)',
+						'Partecipazione gratuita due eventi formativi annuali IG Passivhaus Lombardia (del valore di € 50,00 cadauno)',
+						'Partecipazione eventi esclusivi iscritti IG Passivhaus Lombardia (networking)',
 						'Partecipazione proprio progetto a evento Passive Hour (posti in base alla disponibilità)',
-						'Iscrizione elenco professionisti sito IGP Lombardia'
+						'Iscrizione elenco professionisti sito IG Passivhaus Lombardia'
 					]
 				},
 				registration: {
